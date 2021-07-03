525500f2d034fd908c47d294cd186500566736fb
Junichi Tanaka <junichi200123@gmail.com>
2021-07-04 05:02:56 +0900

    哲学者の作戦を変更
    偶数の哲学者は右->左という順番でフォークを取る
    
    奇数の哲学者は左->右という順番でフォークを取る
    
    これを同時に行ってしまうと, 1人の哲学者しか食べれないというデッドロックに近い状態になってしまうので奇数の哲学者は10ms待つことでこれを避けれるか検証中

--- philosopher_actions.c
+++ philosopher_actions.c
@@ -13,16 +13,20 @@ int	philospher_eat(int philo_idx)
 	/* 哲学者の位置により右手か左手を優先する解法 */
 	if (philo_idx % 2 == 0)
 	{
+		// 哲学者が偶数なら右のフォークを先に取る
 		fork_idx = philo_idx % g_philos_info.num_of_philos;
 		next_fork_idx = (philo_idx + 1) % g_philos_info.num_of_philos;
 	}
 	else
 	{
+		// 哲学者が奇数の場合はスプーンのmutexのブロックを防ぐために10ms待つ
+		usleep(10);
 		fork_idx = (philo_idx + 1) % g_philos_info.num_of_philos;
 		next_fork_idx = philo_idx % g_philos_info.num_of_philos;
 	}
 
 	hold_fork(fork_idx);
+	write_philo_status(philo_idx, HAS_TAKEN_A_FORK);
 	hold_fork(next_fork_idx);
 	write_philo_status(philo_idx, HAS_TAKEN_A_FORK);
 	write_philo_status(philo_idx, EATING);
