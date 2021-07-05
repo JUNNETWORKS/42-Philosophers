e03e3ccb2a2b50700eeb0c58f793b5de4f60bd9e
Junichi Tanaka <junichi200123@gmail.com>
2021-07-06 04:46:23 +0900

    各哲学者1ずつに死活監視スレッドを作成

--- main.c
+++ main.c
@@ -10,32 +10,19 @@ t_philos_info	g_philos_info;
 t_philo			*g_philos;
 pthread_mutex_t	*g_forks;
 
-void	*thr_philosopher(void *arg)
-{
-	long	philo_idx = (long)arg;
-
-	g_philos[philo_idx].status = THINKING;
-	g_philos[philo_idx].last_eating_ms = get_current_time_ms();
-	g_philos[philo_idx].eating_count = 0;
-	while (1)
-	{
-		if (g_philos[philo_idx].status == THINKING)
-			philosopher_eat(philo_idx);
-		else if (g_philos[philo_idx].status == SLEEPING)
-			philosopher_sleep(philo_idx);
-	}
-	return ((void *)0);
-}
-
 int	main(int argc, char **argv)
 {
+	pthread_t	*philo_observers;
+
 	if (parse_philos_argv(argc, argv))
 		return (1);
 	g_philos = malloc(sizeof(t_philo) * g_philos_info.num_of_philos);
 	g_forks = malloc(sizeof(pthread_mutex_t) * g_philos_info.num_of_philos);
-	if (!g_philos || !g_forks || init_g_forks(g_philos_info.num_of_philos)
-		|| init_g_philos())
+	philo_observers = malloc(sizeof(pthread_mutex_t) * g_philos_info.num_of_philos);
+	if (!g_philos || !g_forks || !philo_observers
+		|| init_g_forks(g_philos_info.num_of_philos) || start_g_philos()
+		|| start_philo_observers(philo_observers))
 		return (1);
-	monitor_if_philosophers_are_living();
+	wait_philo_observers(philo_observers);
 	return (0);
 }
--- philosopher.c
+++ philosopher.c
@@ -3,6 +3,24 @@
 #include "philosopher.h"
 #include "utils.h"
 
+static void	*thr_philosopher(void *arg)
+{
+	long	philo_idx;
+
+	philo_idx = (long)arg;
+	g_philos[philo_idx].status = THINKING;
+	g_philos[philo_idx].last_eating_ms = get_current_time_ms();
+	g_philos[philo_idx].eating_count = 0;
+	while (1)
+	{
+		if (g_philos[philo_idx].status == THINKING)
+			philosopher_eat(philo_idx);
+		else if (g_philos[philo_idx].status == SLEEPING)
+			philosopher_sleep(philo_idx);
+	}
+	return ((void *)0);
+}
+
 bool	is_philo_still_alive(t_philo *philo, long time_to_die_ms, long must_eat_times)
 {
 	long	rest_time_ms;
@@ -15,3 +33,20 @@ bool	is_philo_still_alive(t_philo *philo, long time_to_die_ms, long must_eat_tim
 	return (rest_time_ms > 0 || !has_eaten_n_times);
 }
 
+int	start_g_philos(void)
+{
+	long	i;
+
+	i = 0;
+	while (i < g_philos_info.num_of_philos)
+	{
+		g_philos[i].is_living = true;
+		if (pthread_create(&g_philos[i].thread, NULL, thr_philosopher, (void *)i))
+		{
+			printf("pthread_create() error!\n");
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
