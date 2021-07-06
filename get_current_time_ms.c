89bf2856ca9be8bf2c7ca88c738a4723c2a38688
Junichi Tanaka <junichi200123@gmail.com>
2021-07-07 04:24:22 +0900

    usleep()やgettimeofday()が返すのはmsではなくμsだったので修正
    msだと思ってた...

--- get_current_time_ms.c
+++ get_current_time_ms.c
@@ -6,5 +6,5 @@ long	get_current_time_ms(void)
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return (tv.tv_sec * 1000 + tv.tv_usec);
+	return (tv.tv_sec / 1000 + tv.tv_usec * 1000);
 }
--- philosopher_actions.c
+++ philosopher_actions.c
@@ -20,7 +20,7 @@ int	philosopher_eat(int philo_idx)
 	else
 	{
 		// 哲学者が奇数の場合はスプーンのmutexのブロックを防ぐために10ms待つ
-		usleep(10);
+		usleep(10 * 1000);
 		fork_idx = (philo_idx + 1) % g_philos_info.num_of_philos;
 		next_fork_idx = philo_idx % g_philos_info.num_of_philos;
 	}
@@ -32,7 +32,7 @@ int	philosopher_eat(int philo_idx)
 	write_philo_status(philo_idx, HAS_TAKEN_A_FORK, get_current_time_ms());
 	write_philo_status(philo_idx, EATING, get_current_time_ms());
 	g_philos[philo_idx].status = EATING;
-	usleep(g_philos_info.time_to_eat_ms);
+	usleep(g_philos_info.time_to_eat_ms * 1000);
 	release_fork(fork_idx);
 	release_fork(next_fork_idx);
 	g_philos[philo_idx].last_eating_ms = get_current_time_ms();
@@ -44,7 +44,7 @@ int	philosopher_eat(int philo_idx)
 int	philosopher_sleep(int philo_idx)
 {
 	write_philo_status(philo_idx, SLEEPING, get_current_time_ms());
-	usleep(g_philos_info.time_to_sleep_ms);
+	usleep(g_philos_info.time_to_sleep_ms * 1000);
 	g_philos[philo_idx].status = THINKING;
 	write_philo_status(philo_idx, THINKING, get_current_time_ms());
 	return (0);
