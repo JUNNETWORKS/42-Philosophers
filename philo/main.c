08136ba4cf58bdd5c3756c092714aae42919d299
Junichi Tanaka <junichi200123@gmail.com>
2021-07-10 18:10:55 +0900

    philo/ main.c Normエラー修正

--- philo/main.c
+++ philo/main.c
@@ -18,7 +18,8 @@ int	main(int argc, char **argv)
 		return (1);
 	g_philos = malloc(sizeof(t_philo) * g_philos_info.num_of_philos);
 	g_forks = malloc(sizeof(pthread_mutex_t) * g_philos_info.num_of_philos);
-	philo_observers = malloc(sizeof(pthread_mutex_t) * g_philos_info.num_of_philos);
+	philo_observers = malloc(
+			sizeof(pthread_mutex_t) * g_philos_info.num_of_philos);
 	if (!g_philos || !g_forks || !philo_observers
 		|| init_g_forks(g_philos_info.num_of_philos) || start_g_philos()
 		|| start_philo_observers(philo_observers))
