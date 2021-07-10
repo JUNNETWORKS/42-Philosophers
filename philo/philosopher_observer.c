fc5d4e1ca2129fa5ad270f07afc608557e19c636
Junichi Tanaka <junichi200123@gmail.com>
2021-07-10 18:09:57 +0900

    philo/ philosopher_observer.c Normエラー修正

--- philo/philosopher_observer.c
+++ philo/philosopher_observer.c
@@ -32,8 +32,8 @@ int	start_philo_observers(pthread_t	*philo_observers)
 	i = 0;
 	while (i < g_philos_info.num_of_philos)
 	{
-
-		if (pthread_create(&philo_observers[i], NULL, thr_philo_observer, (void *)i))
+		if (pthread_create(&philo_observers[i], NULL,
+				thr_philo_observer, (void *)i))
 		{
 			printf("pthread_create() error!\n");
 			return (-1);
@@ -52,4 +52,3 @@ int	wait_philo_observers(pthread_t	*philo_observers)
 		pthread_join(philo_observers[i++], NULL);
 	return (0);
 }
-
