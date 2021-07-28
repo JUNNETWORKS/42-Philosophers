0a3842a54bbe457137081d811b66328925e1fa85
Junichi Tanaka <junichi200123@gmail.com>
2021-07-29 01:47:21 +0900

    哲学者の位置によって右左どちらを先に取るか決まる手法でうまくいってる

--- philo/philosopher_actions.c
+++ philo/philosopher_actions.c
@@ -8,35 +8,35 @@
 static void	get_fork_idxes(
 	long num_of_forks, int *fork_idx, int *next_fork_idx, int philo_idx)
 {
-//	/* 哲学者の位置により右手か左手を優先する解法 */
-//	if (philo_idx % 2 == 0)
-//	{
-//		// 哲学者が偶数なら右のフォークを先に取る
-//		*fork_idx = philo_idx % num_of_forks;
-//		*next_fork_idx = (philo_idx + 1) % num_of_forks;
-//	}
-//	else
-//	{
-//		// 哲学者が奇数の場合はスプーンのmutexのブロックを防ぐために10us待つ
-//		usleep(10);
-//		*fork_idx = (philo_idx + 1) % num_of_forks;
-//		*next_fork_idx = philo_idx % num_of_forks;
-//	}
-
-	/* リソース階層による解法
-	 * 各哲学者は大きいidxのフォークを最初に取る. その後小さいidxのフォークを取る風にする.
-	 * 食後は小さいフォークを解放してから大きいidxのフォークを解放する.
-	 */
-	if (philo_idx < num_of_forks - 1)
+	/* 哲学者の位置により右手か左手を優先する解法 */
+	if (philo_idx % 2 == 0)
 	{
-		*fork_idx = philo_idx + 1;
-		*next_fork_idx = philo_idx;
+		// 哲学者が偶数なら右のフォークを先に取る
+		*fork_idx = philo_idx % num_of_forks;
+		*next_fork_idx = (philo_idx + 1) % num_of_forks;
 	}
 	else
 	{
-		*fork_idx = 0;
-		*next_fork_idx = num_of_forks - 1;
+		// 哲学者が奇数の場合はスプーンのmutexのブロックを防ぐために10us待つ
+		usleep(10);
+		*fork_idx = (philo_idx + 1) % num_of_forks;
+		*next_fork_idx = philo_idx % num_of_forks;
 	}
+
+// 	/* リソース階層による解法
+// 	 * 各哲学者は大きいidxのフォークを最初に取る. その後小さいidxのフォークを取る風にする.
+// 	 * 食後は小さいフォークを解放してから大きいidxのフォークを解放する.
+// 	 */
+// 	if (philo_idx < num_of_forks - 1)
+// 	{
+// 		*fork_idx = philo_idx + 1;
+// 		*next_fork_idx = philo_idx;
+// 	}
+// 	else
+// 	{
+// 		*fork_idx = 0;
+// 		*next_fork_idx = num_of_forks - 1;
+// 	}
 }
 
 /* リソース階層による解法
@@ -50,16 +50,20 @@ int	philosopher_eat(t_philos_info *philos_info, t_philo *philo)
 
 	get_fork_idxes(philo->philos_info->num_of_philos,
 		&fork_idx, &next_fork_idx, philo->idx);
+	// printf("start hold_fork (%ld): %d\n", philo->idx, fork_idx);
 	hold_fork(philos_info->forks, fork_idx);
+	// printf("end hold_fork (%ld): %d\n", philo->idx, fork_idx);
 	if (is_philo_simulation_ended(philos_info, philo))
 		return (release_forks_and_rtn_err(philos_info->forks, fork_idx, -1));
 	write_philo_status(philo->idx, HAS_TAKEN_A_FORK, get_current_time_ms());
+	// printf("start hold_fork (%ld): %d\n", philo->idx, next_fork_idx);
 	hold_fork(philos_info->forks, next_fork_idx);
+	// printf("end hold_fork (%ld): %d\n", philo->idx, next_fork_idx);
 	if (is_philo_simulation_ended(philos_info, philo))
-		return (release_forks_and_rtn_err(philos_info->forks, fork_idx, next_fork_idx));
+		return (release_forks_and_rtn_err(philos_info->forks, next_fork_idx, fork_idx));
 	write_philo_status(philo->idx, HAS_TAKEN_A_FORK, get_current_time_ms());
 	if (is_philo_simulation_ended(philos_info, philo))
-		return (release_forks_and_rtn_err(philos_info->forks, fork_idx, next_fork_idx));
+		return (release_forks_and_rtn_err(philos_info->forks, next_fork_idx, fork_idx));
 	write_philo_status(philo->idx, EATING, get_current_time_ms());
 	pthread_mutex_lock(&philo->mux);
 	philo->status = EATING;
@@ -68,8 +72,12 @@ int	philosopher_eat(t_philos_info *philos_info, t_philo *philo)
 	usleep(philos_info->time_to_eat_ms * 1000);
 	pthread_mutex_lock(&philo->mux);
 	// release_forks(philos_info->forks, fork_idx, next_fork_idx);
+	printf("start release_fork (%ld): %d\n", philo->idx, next_fork_idx);
 	release_fork(philos_info->forks, next_fork_idx);
+	// printf("end release_fork (%ld): %d\n", philo->idx, next_fork_idx);
+	printf("start release_fork (%ld): %d\n", philo->idx, fork_idx);
 	release_fork(philos_info->forks, fork_idx);
+	// printf("end release_fork (%ld): %d\n", philo->idx, fork_idx);
 	philo->eating_count += 1;
 	philo->status = SLEEPING;
 	pthread_mutex_unlock(&philo->mux);
