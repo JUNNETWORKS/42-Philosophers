5e85b57fa1c02fa6bf23e84b86641854e4786a10
Junichi Tanaka <junichi200123@gmail.com>
2021-07-30 08:27:33 +0900

    philosopher_eat() 内のフォークを取得しようとする部分を別の関数に切り分けた

--- philo/philosopher_actions.c
+++ philo/philosopher_actions.c
@@ -5,10 +5,10 @@
 #include "philosopher.h"
 #include "utils.h"
 
+/* 哲学者の人数によってどういう風にフォークを取るか変える手法 */
 static void	get_fork_idxes(
 	long num_of_forks, int *fork_idx, int *next_fork_idx, int philo_idx)
 {
-	/* 哲学者の人数によってどういう風にフォークを取るか変える手法 */
 	if (num_of_forks % 2 == 0)
 	{
 		if (philo_idx % 2 == 1)
@@ -27,6 +27,25 @@ static void	get_fork_idxes(
 	}
 }
 
+static int	philo_try_2_hold_forks(t_philos_info *philos_info, t_philo *philo,
+	int fork_idx, int next_fork_idx)
+{
+	hold_fork(philos_info->forks, fork_idx);
+	if (is_philo_simulation_ended(philos_info, philo))
+		return (release_forks_and_rtn_err(philos_info->forks, fork_idx, -1));
+	write_philo_status(philo->idx, HAS_TAKEN_A_FORK, get_current_time_ms());
+	hold_fork(philos_info->forks, next_fork_idx);
+	if (is_philo_simulation_ended(philos_info, philo))
+		return (release_forks_and_rtn_err(philos_info->forks,
+				next_fork_idx, fork_idx));
+	write_philo_status(philo->idx, HAS_TAKEN_A_FORK, get_current_time_ms());
+	if (is_philo_simulation_ended(philos_info, philo))
+		return (release_forks_and_rtn_err(philos_info->forks,
+				next_fork_idx, fork_idx));
+	write_philo_status(philo->idx, EATING, get_current_time_ms());
+	return (0);
+}
+
 /* リソース階層による解法
  * 各哲学者は大きいidxのフォークを最初に取る. その後小さいidxのフォークを取る風にする.
  * 食後は小さいフォークを解放してから大きいidxのフォークを解放する.
@@ -38,17 +57,8 @@ int	philosopher_eat(t_philos_info *philos_info, t_philo *philo)
 
 	get_fork_idxes(philo->philos_info->num_of_philos,
 		&fork_idx, &next_fork_idx, philo->idx);
-	hold_fork(philos_info->forks, fork_idx);
-	if (is_philo_simulation_ended(philos_info, philo))
-		return (release_forks_and_rtn_err(philos_info->forks, fork_idx, -1));
-	write_philo_status(philo->idx, HAS_TAKEN_A_FORK, get_current_time_ms());
-	hold_fork(philos_info->forks, next_fork_idx);
-	if (is_philo_simulation_ended(philos_info, philo))
-		return (release_forks_and_rtn_err(philos_info->forks, next_fork_idx, fork_idx));
-	write_philo_status(philo->idx, HAS_TAKEN_A_FORK, get_current_time_ms());
-	if (is_philo_simulation_ended(philos_info, philo))
-		return (release_forks_and_rtn_err(philos_info->forks, next_fork_idx, fork_idx));
-	write_philo_status(philo->idx, EATING, get_current_time_ms());
+	if (philo_try_2_hold_forks(philos_info, philo, fork_idx, next_fork_idx))
+		return (-1);
 	pthread_mutex_lock(&philo->mux);
 	philo->status = EATING;
 	philo->last_eating_ms = get_current_time_ms();
