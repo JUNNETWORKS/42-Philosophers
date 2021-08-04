a924ab7c1f96dcde17a13454490164d14ff2cf9f
Junichi Tanaka <junichi200123@gmail.com>
2021-08-04 19:39:44 +0900

    allocate philos_info in heap area

--- philo/main.c
+++ philo/main.c
@@ -9,24 +9,25 @@
 int	main(int argc, char **argv)
 {
 	pthread_t		*philo_observers;
-	t_philos_info	philos_info;
+	t_philos_info	*philos_info;
 	t_philo			*philos;
 
-	if (parse_philos_argv(argc, argv, &philos_info))
+	philos_info = malloc(sizeof(t_philos_info));
+	if (!philos_info || parse_philos_argv(argc, argv, philos_info))
 		return (1);
-	philos = malloc(sizeof(t_philo) * philos_info.num_of_philos);
-	philos_info.forks = malloc(
-			sizeof(pthread_mutex_t) * philos_info.num_of_philos);
-	philos_info.end_of_simulation = false;
+	philos = malloc(sizeof(t_philo) * philos_info->num_of_philos);
+	philos_info->forks = malloc(
+			sizeof(pthread_mutex_t) * philos_info->num_of_philos);
+	philos_info->end_of_simulation = false;
 	philo_observers = malloc(
-			sizeof(pthread_t) * philos_info.num_of_philos);
-	if (!philos || !philos_info.forks || !philo_observers
-		|| pthread_mutex_init(&philos_info.mux, NULL)
-		|| init_forks(philos_info.forks, philos_info.num_of_philos)
-		|| init_philos(&philos_info, philos)
-		|| start_philos(&philos_info, philos)
-		|| start_philo_observers(&philos_info, philos, philo_observers))
+			sizeof(pthread_t) * philos_info->num_of_philos);
+	if (!philos || !philos_info->forks || !philo_observers
+		|| pthread_mutex_init(&philos_info->mux, NULL)
+		|| init_forks(philos_info->forks, philos_info->num_of_philos)
+		|| init_philos(philos_info, philos)
+		|| start_philos(philos_info, philos)
+		|| start_philo_observers(philos_info, philos, philo_observers))
 		return (1);
-	wait_philo_observers(&philos_info, philo_observers);
+	wait_philo_observers(philos_info, philo_observers);
 	return (0);
 }
